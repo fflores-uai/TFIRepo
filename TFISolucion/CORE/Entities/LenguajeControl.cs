137a706d0b1d03b42092b72d62f44c22914a97b0
fflores-uai <francisco.floresfanelli@gmail.com>
2016-10-17 08:37:38 -0300

    Global Asax + Translator

--- TFISolucion/CORE/Entities/LenguajeControl.cs
+++ TFISolucion/CORE/Entities/LenguajeControl.cs
@@ -3,7 +3,7 @@
     public class LenguajeControl
     {
         public int LenguajeID { get; set; }
-        public string Texto { get; set; }
+        public string Control { get; set; }
         public string Valor { get; set; }
 
         public Lenguaje Lenguaje { get; set; }
---
+++ TFISolucion/GUI/Helpers/TranslateHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace TFI.GUI.Helpers
+{
+    public static class TranslateHelper
+    {
+        public static string Translate(this string value, string language = null)
+        {
+            var lang = language != null ? language : "ES";
+
+
+
+            return null;
+        }
+    }
+}
