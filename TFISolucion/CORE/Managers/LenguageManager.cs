5a3e6deedceee8c1f8b68764f405bf3f71b41e2a
fflores-uai <francisco.floresfanelli@gmail.com>
2016-10-20 13:29:09 -0300

    Agregado Language Manager

---
+++ TFISolucion/CORE/Managers/LenguageManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TFI.CORE.Managers
+{
+    public class LenguageManager
+    {
+        public static IDictionary<string, string> GetDiccionario(string language)
+        {
+            var dic = new Dictionary<string, string>();
+
+            dic.Add("nombre", "Name");
+            dic.Add("apellido", "Apellido");
+
+            return dic;
+        }
+    }
+}
--- TFISolucion/GUI/Helpers/CultureManager.cs
+++ TFISolucion/GUI/Helpers/CultureManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Web;
+using TFI.CORE.Managers;
 
 namespace TFI.GUI.Helpers
 {
@@ -9,6 +11,9 @@ namespace TFI.GUI.Helpers
         private const string KEY = "lang";
         private static HttpContext Context { get { return HttpContext.Current; } set { } }
 
+        private static IDictionary<string, string> DIC;
+
+        // Opcional
         public static string CookieCulture
         {
             get { return Context.Request.Cookies[KEY] == null ? (string)null : Context.Request.Cookies[KEY].Value; }
@@ -16,7 +21,7 @@ namespace TFI.GUI.Helpers
         }
 
         /// <summary>
-        /// Cambia el valor de lenguage de la pagina.
+        /// Cambia el valor de lenguage de la pagina, lo guarda en session y ademas cambia el Culture.
         /// </summary>
         /// <param name="language"></param>
         public static void UpdateCulture(string language = null)
@@ -29,6 +34,13 @@ namespace TFI.GUI.Helpers
             CultureInfo culture = new CultureInfo(lang);
             System.Threading.Thread.CurrentThread.CurrentCulture = culture;
             System.Threading.Thread.CurrentThread.CurrentUICulture = culture;
+        }
+
+        public static string Translate(this string value)
+        {
+            DIC = LenguageManager.GetDiccionario(KEY);
+
+            return value;
         }
     }
 }
