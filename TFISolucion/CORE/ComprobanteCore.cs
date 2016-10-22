1ff237ddee7898f5b6850a7938616aecc8735f29
galianoleo <galianoleo@gmail.com>
2016-10-22 14:30:36 -0300

    subida inicial

---
+++ TFISolucion/CORE/ComprobanteCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFI.DAL.DAL;
+using TFI.Entidades;
+
+namespace TFI.CORE
+{
+    public class ComprobanteCore
+    {
+    }
+}
